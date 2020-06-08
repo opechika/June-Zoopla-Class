62a4e17834065b48497ad960b6f06cc1fb5a6fa3
sayoafolabi <sayo_folabi@hotmail.com>
2020-06-08 19:12:41 +0100

    making further changes to stepdefinition

--- src/test/java/co/uk/zoopla/stepDefinitions/ForSaleSearchSteps.java
+++ src/test/java/co/uk/zoopla/stepDefinitions/ForSaleSearchSteps.java
@@ -21,5 +21,40 @@ public class ForSaleSearchSteps {
 
     }
 
+    @When("I select {string} from Max price")
+    public void i_select_from_Max_price(String string) {
+
+    }
+
+    @When("I select {string} from Property type")
+    public void i_select_from_Property_type(String string) {
+
+    }
+
+    @When("I select {string} from Bedrooms")
+    public void i_select_from_Bedrooms(String string) {
+
+    }
+
+    @When("I click on Search button")
+    public void i_click_on_Search_button() {
+
+    }
+
+    @Then("the search for {string} is displayed")
+    public void the_search_for_is_displayed(String string) {
+
+    }
+
+    @Then("I click on one of the search result")
+    public void i_click_on_one_of_the_search_result() {
+
+    }
+
+    @Then("an error page is displayed")
+    public void an_error_page_is_displayed() {
+
+    }
+
 }
 
